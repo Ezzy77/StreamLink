01cfa9acbe0a1bda565c122b485faaf16679d917
Elisio <racabral11@icloud.com>
2024-08-13 14:48:23 +0100

    first

---
+++ src/main/java/com/elisiocabral/stream_link/StreamLinkApplication.java
@@ -0,0 +1,13 @@
+package com.elisiocabral.stream_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamLinkApplication.class, args);
+	}
+
+}
